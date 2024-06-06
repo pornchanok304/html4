981d9b108565666aabef0987569db028298c6add
pornchanok304 <158019721+pornchanok304@users.noreply.github.com>
2024-06-06 22:01:18 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,156 @@
+var tourpoint = 
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "khao yai nationnal park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.50412473478372,
+            14.333888677747652
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Grand Palace",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49156239677751,
+            13.751562922126496
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Icon siam",
+          "type": "spacial",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.51043650000389,
+            13.726822672510593
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Dream Wolrd",
+          "type": "spacial",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.67521867803265,
+            13.991638869672812
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao Laem Ya-Mu ko Samet National Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.45370595991892,
+            12.568161559786944
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khuean Srinagarindra National Park",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            99.04059158268126,
+            14.741906736460365
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Phrathat Doi Suthep",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            98.92208086816896,
+            18.804943917445584
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Phu Kradueng National Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.76024608106246,
+            16.886904124775285
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Amphawa Floating Market",
+          "type": "spacial",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            99.95540778422964,
+            13.424711178278244
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Doi inthanon",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            98.48769096919625,
+            18.58812104663926
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
